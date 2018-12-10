50368cd9cd66e22ad1a9d690f139dbdf7bfb3858
agatatok <agattok260@student.polsl.pl>
2018-12-10 21:19:01 +0100

    Pierwszy commit. Wrzucenie plików.

---
+++ WpfUI/Analizer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WpfUI
+{
+    class Analizer
+    {
+        private string source;
+        private string[] text;
+        public string Source { get => source; set => source = value; }
+
+        public Analizer(string source)
+        {
+            this.source = source;
+        }
+
+        
+        public void Analize()
+        {
+            Read();
+            Payment[] payments = new Payment[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                string[] oneLine = text[i].Split('|');
+                double sumDouble = Convert(oneLine[5]);
+                if (sumDouble > 0)
+                {
+                    payments[i] = new Payment(oneLine[3], oneLine[2], Convert(oneLine[5]));
+                }
+            }
+
+        }
+
+
+        private double Convert(string sum)
+        {
+            if (sum.StartsWith("-"))
+            {
+                return 0;
+            }
+            else
+            {
+                sum = sum.Replace(',', '.');
+                return Double.Parse(sum, System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+
+
+        private void Read()
+        {
+            int linesCount = 0;
+            using (StreamReader sr = new StreamReader(source))
+            {
+                while (sr.ReadLine() != null) { linesCount++; }
+            }
+
+            text = new string[linesCount - 1];
+
+            using (StreamReader sr = new StreamReader(source))
+            {
+                sr.ReadLine();
+                for (int i = 0; i < text.Length; i++)
+                {
+                    text[i] = sr.ReadLine();
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ WpfUI/Database.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Entity.Core.Objects;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Data.SqlServerCe;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace WpfUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Baza.xaml
+    /// </summary>
+    public partial class Database : Window
+    {
+        static ObservableCollection<Person> people = new ObservableCollection<Person>();
+        public static ObservableCollection<Person> People
+        {
+            get
+            {
+                return people;
+            }
+        }
+        
+        public Database()
+        {
+           InitializeComponent();
+            BindDataGrid();
+
+        }
+
+        private void BindDataGrid()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["connstrPlatnicy"].ConnectionString;
+            connection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from [Płatnicy]";
+            cmd.Connection = connection;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("Płatnicy");
+            da.Fill(dt);
+
+            dg_Baza.ItemsSource = dt.DefaultView;
+        }
+
+        private void AddPersonIntoBase_Click(object sender, RoutedEventArgs e)
+        {
+            AddPerson window1 = new AddPerson();
+            window1.Show();
+
+        }
+        
+
+    }
+}
